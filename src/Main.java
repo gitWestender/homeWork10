b62acc09e6e9202dac54a79ed588e2cefed298c5
Westender <javaxwestender@gmail.com>
2023-08-07 15:57:24 +0300

    Add: task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
-    public static void main(String[] args) {
 
+    static int counter = 1;
+    public static void main(String[] args) {
+        task1();
+    }
+
+    static void task1() {
+        System.out.printf("\nЗадание %d:", counter++);
+
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName = "Ivanovich";
+        String fullName = firstName + " " + middleName + " " + lastName;
+
+        System.out.printf("\nФИО сотрудника — %s\n", fullName);
     }
 }
